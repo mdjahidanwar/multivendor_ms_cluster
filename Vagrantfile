4b046145a9f10cce09f8e654589c5eb644a7e8a1
mdjahidanwar <mdjahidanwar1995@gmail.com>
2024-01-22 20:13:04 +0600

    for redisearch

--- Vagrantfile
+++ Vagrantfile
@@ -23,8 +23,8 @@ Vagrant.configure("2") do |config|
       #node.ssh.private_key_path = "~/.ssh/id_rsa"
       node.ssh.forward_agent = true
 
-      #node.vm.network  "public_network", bridge: "bridge0", ip: "#{ip_prefix}#{i}"
-      node.vm.network  "public_network",bridge: "Intel(R) 82579LM Gigabit Network Connection", ip: "#{ip_prefix}#{i}"
+      node.vm.network  "public_network", bridge: "bridge0", ip: "#{ip_prefix}#{i}"
+      #node.vm.network  "public_network",bridge: "Intel(R) 82579LM Gigabit Network Connection", ip: "#{ip_prefix}#{i}"
       node.vm.provider "#{provider}" do |vm|
         vm.memory = "#{memory}"
         vm.cpus = "#{cpus}"
--- bootstrap.sh
+++ bootstrap.sh
@@ -31,50 +31,21 @@ echo "lxc.idmap = g 0 $MS_GID $ME_GID" >> /home/admin/.config/lxc/default.conf
 
 cat > /home/admin/create-containers.sh << EOF
 
-#!/bin/bash
+lxc-create -n  a0420psmygpdaredis07 -t download --  --dist centos --release 8 --arch amd64 
+lxc-create -n a0420psmygpdaredis08     -t download --  --dist centos --release 8 --arch amd64 
+lxc-create -n a0420psmygpdaredis09  -t download --  --dist centos --release 8 --arch amd64 
 
-# nodes=( a0420smmcmsdamonitor01 a0420snemcmsdalb01 a0420snemcmsdalb02 a0420ssecdapmcmscas01 a0420ssecdapmcmscas02 a0420ssecdapmcmscas03 a0420ssecmcmsdaes01 a0420ssecmcmsdaes02 a0420ssecmcmsdaes03  a0420ssecmcmsdarmq01 a0420ssecmcmsdarmq02 )
-# for i in "${nodes[@]}"
-# do
-# echo "creating container $i --->>"
-# lxc-create -n  $i -t download --  --dist centos --release 7 --arch amd64 
-# lxc-create -n  $i -t download --  --dist centos --release 7 --arch amd64 
-# done
 
-lxc-create -n  a0420smmcmsdamonitor01 -t download --  --dist centos --release 7 --arch amd64 
-lxc-create -n a0420snemcmsdalb01     -t download --  --dist centos --release 7 --arch amd64 
-lxc-create -n a0420snemcmsdalb02  -t download --  --dist centos --release 7 --arch amd64 
-lxc-create -n a0420ssecdapmcmscas01  -t download --  --dist centos --release 7 --arch amd64 
-lxc-create -n a0420ssecdapmcmscas02  -t download --  --dist centos --release 7 --arch amd64 
-lxc-create -n a0420ssecdapmcmscas03  -t download --  --dist centos --release 7 --arch amd64 
-lxc-create -n a0420ssecmcmsdaes01  -t download --  --dist centos --release 7 --arch amd64 
-lxc-create -n a0420ssecmcmsdaes02  -t download --  --dist centos --release 7 --arch amd64 
-lxc-create -n a0420ssecmcmsdaes03  -t download --  --dist centos --release 7 --arch amd64 
-lxc-create -n a0420ssecmcmsdarmq01  -t download --  --dist centos --release 7 --arch amd64 
-lxc-create -n a0420ssecmcmsdarmq02    -t download --  --dist centos --release 7 --arch amd64 
 
 echo "taking 5mins rest--->"
 sleep 5
 
 echo -e "container created\nstarting the containers---->"
-# for i in "${nodes[@]}"
-# do
-# echo "creating container $i --->>"
-# lxc-start -n  $i 
-# done
 
 
-lxc-start -n  a0420smmcmsdamonitor01 
-lxc-start -n a0420snemcmsdalb01   
-lxc-start -n a0420snemcmsdalb02   
-lxc-start -n a0420ssecdapmcmscas01   
-lxc-start -n a0420ssecdapmcmscas02   
-lxc-start -n a0420ssecdapmcmscas03   
-lxc-start -n a0420ssecmcmsdaes01   
-lxc-start -n a0420ssecmcmsdaes02   
-lxc-start -n a0420ssecmcmsdaes03   
-lxc-start -n a0420ssecmcmsdarmq01  
-lxc-start -n a0420ssecmcmsdarmq02  
+lxc-start -n  a0420psmygpdaredis07 
+lxc-start -n a0420psmygpdaredis08   
+lxc-start -n a0420psmygpdaredis09
 
 lxc-ls -f 
 
@@ -83,47 +54,10 @@ EOF
 chmod +x /home/admin/create-containers.sh
 /home/admin/create-containers.sh 
 
-echo "docker compose --->"
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-
-sudo usermod -aG docker admin
-
-echo "installing kind------>"
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.14.0/kind-linux-amd64
-chmod +x ./kind
-sudo mv ./kind /bin/kind
-git clone https://github.com/mdjahidanwar/kind-cluster.git
-
-echo "ansible -->"
-sudo apt-add-repository ppa:ansible/ansible
-sudo apt update
-sudo apt install ansible -y 
-
-echo "[nodes]" > /etc/ansible/hosts
-
 sleep 10
 echo "inserting values on hosts files"
 lxc-ls -f | awk 'NR>1{print $5,$1}' >> /etc/hosts
 lxc-ls -f | awk 'NR>1{print $1}' > /etc/ansible/hosts
 
-sed -i '/^a0420smmcmsdamonitor01.*/i [monitor]' /etc/ansible/hosts
-sed -i '/^a0420snemcmsdalb01.*/i [lb]' /etc/ansible/hosts
-sed -i '/^a0420ssecdapmcmscas01.*/i [cas]' /etc/ansible/hosts
-sed -i '/^a0420ssecmcmsdaes01.*/i [es]' /etc/ansible/hosts
-sed -i '/^a0420ssecmcmsdarmq01.*/i [mq]' /etc/ansible/hosts
-
 echo "generating ssh key pair"
-ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa <<< y
-
-
-
-
-
-#start=0
-#while [[ $start -le $3 ]]
-#do 
-#echo "$1$3 ansible_host=$2$3" >> /etc/ansible/hosts
-#echo "$1$3 $2$3" >> /etc/hosts
-#(( start = start + 1))
-#done
+sudo -u admin ssh-keygen -t rsa -N '' -f /home/admin/.ssh/id_rsa <<< y
